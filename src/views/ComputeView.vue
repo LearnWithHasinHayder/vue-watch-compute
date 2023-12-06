4330591364eb4faa3ed5b28537ae2932a5eaa84b
Hasin Hayder <countdraculla@gmail.com>
2023-12-06 11:54:42 +0600

    Computed Cache

--- src/views/ComputeView.vue
+++ src/views/ComputeView.vue
@@ -1,23 +1,49 @@
 <script setup>
 import { ref, computed } from 'vue'
-const n1 = ref(0)
-const n2 = ref(0)
+const n1 = ref(4)
+const n2 = ref(12)
+
 
 const getTotal = () => {
+    console.log('getTotal called')
     return n1.value + n2.value
 }
 
-const computedTotal = computed(() => n1.value + n2.value)
+const computedTotal = computed(() => {
+    console.log('computedTotal called')
+    return n1.value + n2.value
+})
 
 </script>
  
 <template>
-    <div class="flex flex-col space-y-2">
-        <h1>Compute</h1>
-        <input type="number" v-model="n1" />
-        <input type="number" v-model="n2" />
+    <div class="">
+        <h1 class="text-2xl mb-5">Computed Cache</h1>
+        <p>N1 = {{ n1 }}</p>
+        <p>N2 = {{ n2 }}</p>
+        <div class="flex flex-between mt-5">
 
-        <p>Result: {{ computedTotal }}</p>
+            <div class="flex space-y-4 flex-col w-1/2">
+                <p>Result {{ getTotal() }}</p>
+                <p>Result {{ getTotal() }}</p>
+                <p>Result {{ getTotal() }}</p>
+                <p>Result {{ getTotal() }}</p>
+                <p>Result {{ getTotal() }}</p>
+            </div>
+            <div class="flex space-y-4 flex-col">
+                <div class="flex space-y-4 flex-col">
+                    <p>Result {{ computedTotal }}</p>
+                    <p>Result {{ computedTotal }}</p>
+                    <p>Result {{ computedTotal }}</p>
+                    <p>Result {{ computedTotal }}</p>
+                    <p>Result {{ computedTotal }}</p>
+                </div>
+            </div>
+
+        </div>
+        <button @click="n1=2;n2=6" class="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Change Number
+        </button>
     </div>
 </template>
  
