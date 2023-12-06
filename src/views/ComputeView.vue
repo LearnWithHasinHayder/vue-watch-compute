3396187eec16b40a26328317f9b9af2c52441739
Hasin Hayder <countdraculla@gmail.com>
2023-12-06 11:34:22 +0600

    Episode 01

--- src/views/ComputeView.vue
+++ src/views/ComputeView.vue
@@ -1,9 +1,24 @@
 <script setup>
+import { ref, computed } from 'vue'
+const n1 = ref(0)
+const n2 = ref(0)
+
+const getTotal = () => {
+    return n1.value + n2.value
+}
+
+const computedTotal = computed(() => n1.value + n2.value)
 
 </script>
  
 <template>
-    <h1>Compute</h1>
+    <div class="flex flex-col space-y-2">
+        <h1>Compute</h1>
+        <input type="number" v-model="n1" />
+        <input type="number" v-model="n2" />
+
+        <p>Result: {{ computedTotal }}</p>
+    </div>
 </template>
  
 <style scoped></style>
