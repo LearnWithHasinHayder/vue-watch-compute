f1d9ba872ad7fc6e262b29be05cf85a9e506b121
Hasin Hayder <countdraculla@gmail.com>
2023-12-06 13:08:28 +0600

    Watch

--- src/views/ComputeView.vue
+++ src/views/ComputeView.vue
@@ -1,49 +1,55 @@
 <script setup>
-import { ref, computed } from 'vue'
-const n1 = ref(4)
-const n2 = ref(12)
+import { ref, watch, onBeforeMount, watchEffect } from 'vue'
 
+const postId = ref(1)
+const post = ref({});
 
-const getTotal = () => {
-    console.log('getTotal called')
-    return n1.value + n2.value
-}
+const postIds = ref([1])
 
-const computedTotal = computed(() => {
-    console.log('computedTotal called')
-    return n1.value + n2.value
+watch(postId, (newValue, oldValue) => {
+    console.log(`Post ID changed from ${oldValue} to ${newValue}`) 
 })
 
+
+onBeforeMount(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId.value}`)
+        .then(response => response.json())
+        .then(json => post.value = json)
+})
+
+const previousPost = () => {
+    postId.value <= 0 ? postId.value-- : postId.value = 1
+    postIds.value.push(postId.value)
+}
+const nextPost = () => {
+    postId.value++
+    postIds.value.push(postId.value)
+}
 </script>
  
 <template>
-    <div class="">
-        <h1 class="text-2xl mb-5">Computed Cache</h1>
-        <p>N1 = {{ n1 }}</p>
-        <p>N2 = {{ n2 }}</p>
-        <div class="flex flex-between mt-5">
-
-            <div class="flex space-y-4 flex-col w-1/2">
-                <p>Result {{ getTotal() }}</p>
-                <p>Result {{ getTotal() }}</p>
-                <p>Result {{ getTotal() }}</p>
-                <p>Result {{ getTotal() }}</p>
-                <p>Result {{ getTotal() }}</p>
-            </div>
-            <div class="flex space-y-4 flex-col">
-                <div class="flex space-y-4 flex-col">
-                    <p>Result {{ computedTotal }}</p>
-                    <p>Result {{ computedTotal }}</p>
-                    <p>Result {{ computedTotal }}</p>
-                    <p>Result {{ computedTotal }}</p>
-                    <p>Result {{ computedTotal }}</p>
-                </div>
-            </div>
+    <div>
+        <div class="flex flex-col space-y-3">
+            <h1 class="text-2xl mb-5">Watch Example</h1>
+            <p>
+                <strong>Post ID:</strong> {{ postId }}
+            </p>
+            <p>
+                <strong>Post Title:</strong> {{ post.title }}
+            </p>
+            <p>
+                <strong>Post Body:</strong> {{ post.body }}
+            </p>
 
         </div>
-        <button @click="n1=2;n2=6" class="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Change Number
-        </button>
+        <div class="mt-5">
+            <button @click="previousPost()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Previous Post
+            </button>
+            <button @click="nextPost()" class="ml-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Next Post
+            </button>
+        </div>
     </div>
 </template>
  
