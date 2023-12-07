9b19b328b2c210ca80658c4bde2160c264d80ad4
Hasin Hayder <countdraculla@gmail.com>
2023-12-07 16:25:20 +0600

    Computed Intro

--- src/views/ComputeView.vue
+++ src/views/ComputeView.vue
@@ -3,12 +3,17 @@ import { ref, computed } from 'vue'
 const n1 = ref(0)
 const n2 = ref(0)
 
-const getTotal = () => {
+
+function getTotal() {
     return n1.value + n2.value
 }
 
-const computedTotal = computed(() => n1.value + n2.value)
 
+// const computedTotal = computed(()=>n1.value + n2.value)
+
+const computedTotal = computed(() => {
+    return n1.value + n2.value
+})
 </script>
  
 <template>
@@ -17,6 +22,7 @@ const computedTotal = computed(() => n1.value + n2.value)
         <input type="number" v-model="n1" />
         <input type="number" v-model="n2" />
 
+        <p>Result: {{ getTotal() }}</p>
         <p>Result: {{ computedTotal }}</p>
     </div>
 </template>
